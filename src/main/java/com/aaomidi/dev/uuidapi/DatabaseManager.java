fc125d660a435c88cd22454ef4029f920d0b2dbf
Amir Omidi <amir@aaomidi.com>
2014-04-16 18:28:04 +0430

    Re-coded to work better

--- src/main/java/com/aaomidi/dev/uuidapi/DatabaseManager.java
+++ src/main/java/com/aaomidi/dev/uuidapi/DatabaseManager.java
@@ -18,10 +18,17 @@ public class DatabaseManager {
         databaseAPI = _plugin.getDatabaseAPI();
     }
 
+    /**
+     * Construct tables to be used in UUID API
+     *
+     * @return Table construction status.
+     */
     private boolean createTables() {
-        String query = "CREATE TABLE IF NOT EXISTS `uuid_api`(`uuid` VARCHAR(32) NOT NULL, `username` VARCHAR(16), `isMain` BOOLEAN, PRIMARY KEY (`uuid`,`userName`))ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+        String query1 = "CREATE TABLE IF NOT EXISTS `uuid_index` (`id` INT NOT NULL AUTO_INCREMENT, `uuid` VARCHAR(32) UNIQUE NOT NULL, PRIMARY KEY (`id`))ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+        String query2 = "CREATE TABLE IF NOT EXISTS `uuid_entries`(`id` INT NOT NULL, `username` VARCHAR(16), `isMain` BOOLEAN, PRIMARY KEY (`id`,`username`))ENGINE=InnoDB DEFAULT CHARSET=utf8;";
         try {
-            databaseAPI.executeUpdate(query);
+            databaseAPI.executeUpdate(query1);
+            databaseAPI.executeUpdate(query2);
             return true;
         } catch (Exception ex) {
             _plugin.getLogger().log(Level.SEVERE, "ERROR OCCURRED WHEN CREATING THE TABLE. \n StackTrace: " + ex.getMessage() + "\nDisabling plugin.");
@@ -30,24 +37,74 @@ public class DatabaseManager {
         }
     }
 
+    /**
+     * Initialize User
+     *
+     * @param player Player to initialize.
+     * @return The initialization process.
+     */
     protected boolean initializeUser(Player player) {
-        String query = "INSERT IGNORE INTO `uuid_api` (`uuid`,`username`,`isMain`) VALUES (?,?,?);";
         String uuid = player.getUniqueId().toString().replace("-", "");
-        this.setAllOldUsernamesInactive(uuid);
-        try {
-            databaseAPI.executeUpdate(query, uuid, player.getName(), true);
-            return true;
-        } catch (Exception ex) {
-            _plugin.getLogger().log(Level.SEVERE, "ERROR OCCURRED WHEN INITIALIZING THE PLAYER " + player.getName() + "\nStackTrace: " + ex.getMessage() + "\nDisabling plugin");
-            _plugin.getServer().getPluginManager().disablePlugin(_plugin);
-            return false;
+        String query1 = "INSERT IGNORE INTO `uuid_index` (`uuid`) VALUES (?)";
+        String query2 = "INSERT IGNORE INTO `uuid_entries` (`id`,`username`,`isMain`) VALUES (?,?,?);";
+        int userIndex = this.getUserIndex(uuid);
+        if (userIndex != -1) {
+            try {
+                databaseAPI.executeQuery(query1, uuid);
+                userIndex = this.getUserIndex(uuid);
+                databaseAPI.executeUpdate(query2, userIndex, player.getName(), true);
+                return true;
+            } catch (Exception ex) {
+                _plugin.getLogger().log(Level.SEVERE, "ERROR OCCURRED WHEN INITIALIZING THE PLAYER " + player.getName() + "\nStackTrace: " + ex.getMessage() + "\nDisabling plugin");
+                _plugin.getServer().getPluginManager().disablePlugin(_plugin);
+                return false;
+            }
+        } else {
+            try {
+                this.setAllOldUsernamesInactive(uuid);
+                databaseAPI.executeUpdate(query2, userIndex, player.getName(), true);
+                return true;
+            } catch (Exception ex) {
+                _plugin.getLogger().log(Level.SEVERE, "ERROR OCCURRED WHEN INITIALIZING THE PLAYER " + player.getName() + "\nStackTrace: " + ex.getMessage() + "\nDisabling plugin");
+                _plugin.getServer().getPluginManager().disablePlugin(_plugin);
+                return false;
+            }
         }
     }
 
-    private boolean setAllOldUsernamesInactive(String uuid) {
-        String query = "SET `isMain`=? WHERE `uuid`=? AND `isMain`=?";
+    /**
+     * Get indexed ID for UUID.
+     *
+     * @param uuid UUID of player.
+     * @return The indexed value.
+     */
+    private int getUserIndex(String uuid) {
+        String query = "SELECT * FROM `uuid_index` WHERE `id`=?";
         try {
-            databaseAPI.executeUpdate(query, false, uuid, true);
+            ResultSet resultSet = databaseAPI.executeQuery(query, uuid);
+            if (resultSet.next()) {
+                do {
+                    return resultSet.getInt("id");
+                } while (resultSet.next());
+            } else {
+                return -1;
+            }
+        } catch (SQLException ex) {
+            return -1;
+        }
+    }
+
+    /**
+     * Set all the old usernames as inactive.
+     *
+     * @param uuid UUID of player.
+     * @return status.
+     */
+    private boolean setAllOldUsernamesInactive(String uuid) {
+        String query = "SET `isMain`=? WHERE `id`=? AND `isMain`=?";
+        Integer userIndex = this.getUserIndex(uuid);
+        try {
+            databaseAPI.executeUpdate(query, false, userIndex, true);
             return true;
         } catch (Exception ex) {
             _plugin.getLogger().log(Level.SEVERE, "ERROR OCCURRED WHEN SETTING THE isMain to FALSE.\n StackTrace: " + ex.getMessage() + "\nDisabling plugin.");
@@ -57,11 +114,18 @@ public class DatabaseManager {
 
     }
 
-    protected ArrayList<String> getOldUsernames(String uuid) {
-        String query = "SELECT `username` FROM `uuid_api`WHERE `uuid`=?";
+    /**
+     * Get usernames.
+     *
+     * @param uuid UUID of player.
+     * @return ArrayList containing ALL usernames
+     */
+    protected ArrayList<String> getUsernames(String uuid) {
+        String query = "SELECT `username` FROM `uuid_api`WHERE `id`=?";
+        Integer userIndex = this.getUserIndex(uuid);
         ArrayList<String> usernames = new ArrayList<>();
         try {
-            ResultSet resultSet = databaseAPI.executeQuery(uuid);
+            ResultSet resultSet = databaseAPI.executeQuery(query, userIndex);
             if (resultSet.next()) {
                 int x = 1;
                 do {
@@ -81,10 +145,17 @@ public class DatabaseManager {
         }
     }
 
-    protected String getActiveUsername(String uuid) {
-        String query = "SELECT `username` FROM `uuid_api` WHERE `uuid`=? AND `isMain`=?";
+    /**
+     * Get the active username of Player.
+     *
+     * @param uuid UUID of player.
+     * @return Active username of Player.
+     */
+    private String getAUsername(String uuid) {
+        String query = "SELECT `username` FROM `uuid_api` WHERE `id`=? AND `isMain`=?";
+        Integer userIndex = this.getUserIndex(uuid);
         try {
-            ResultSet resultSet = databaseAPI.executeQuery(query, uuid, true);
+            ResultSet resultSet = databaseAPI.executeQuery(query, userIndex, true);
             if (resultSet.next()) {
                 do {
                     return resultSet.getString("username");
@@ -99,7 +170,34 @@ public class DatabaseManager {
         }
     }
 
+    /**
+     * Initialize player.
+     *
+     * @param player Player to be initialized.
+     */
     public boolean initializePlayer(Player player) {
         return this.initializeUser(player);
+    }
+
+    /**
+     * Get all usernames of a UUID.
+     *
+     * @param uuid UUID of a player.
+     */
+    protected ArrayList<String> getAllUsernames(String uuid) {
+        return this.getUsernames(uuid);
+    }
+
+    /**
+     * Get active username
+     *
+     * @param uuid UUID of player.
+     */
+    protected String getActiveUsername(String uuid) {
+        return this.getAUsername(uuid);
+    }
+
+    protected int getUI(String uuid) {
+        return this.getUserIndex(uuid);
     }
 }
--- src/main/java/com/aaomidi/dev/uuidapi/UUIDAPI.java
+++ src/main/java/com/aaomidi/dev/uuidapi/UUIDAPI.java
@@ -1,13 +1,17 @@
 package com.aaomidi.dev.uuidapi;
 
 
+import com.aaomidi.dev.uuidapi.events.LoginEvent;
 import com.aaomidi.dev.uuidapi.storage.DatabaseAPI;
 import com.aaomidi.dev.uuidapi.storage.DatabaseConnector;
 import com.aaomidi.dev.uuidapi.utils.ConfigReader;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
 
 public class UUIDAPI extends JavaPlugin {
     @Getter
@@ -18,13 +22,36 @@ public class UUIDAPI extends JavaPlugin {
     private DatabaseAPI databaseAPI;
     @Getter
     private DatabaseManager databaseManager;
+    @Getter
+    private LoginEvent loginEvent;
 
     public final void onEnable() {
-
+        if (!new File(this.getDataFolder(), "config.yml").exists()) {
+            this.saveDefaultConfig();
+        }
+        boolean value = true;
+        if (!this.getServer().getOnlineMode()) {
+            this.getLogger().log(Level.SEVERE, "Server is not running in online mode. This plugin requires online mode. Turning plugin off.");
+            value = false;
+        }
+        if (value) {
+            configReader = new ConfigReader(this);
+            try {
+                databaseConnector = new DatabaseConnector(this);
+            } catch (SQLException e) {
+                this.getLogger().log(Level.SEVERE, "Connecting to database was problematic. Disabling plugin to stop future crashes.");
+                this.getServer().getPluginManager().disablePlugin(this);
+            }
+            databaseAPI = new DatabaseAPI(this);
+            loginEvent = new LoginEvent(this);
+            this.getServer().getPluginManager().registerEvents(loginEvent, this);
+        } else {
+            this.getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     public final void onDisable() {
-
+        databaseAPI.disconnect();
     }
 
     /**
@@ -33,13 +60,13 @@ public class UUIDAPI extends JavaPlugin {
      * @param identifier Name of Player (Player MUST be online) / UUID of player (Enter without dashes).
      * @return ArrayList with the history of usernames.
      */
-    public ArrayList<String> getUsernameHistory(String identifier) {
+    public ArrayList<String> getUsernames(String identifier) {
         if (identifier.length() == 32) {
-             return this.getDatabaseManager().getOldUsernames(identifier);
+            return this.getDatabaseManager().getAllUsernames(identifier);
         } else {
             if (this.getServer().getPlayer(identifier) != null) {
                 String uuid = this.getServer().getPlayer(identifier).getUniqueId().toString().replace("-", "");
-                return this.getUsernameHistory(uuid);
+                return this.getUsernames(uuid);
             } else {
                 return null;
             }
@@ -49,11 +76,21 @@ public class UUIDAPI extends JavaPlugin {
     /**
      * Get active username of a uuid.
      *
-     * @param uuid Unique identifier.
+     * @param uuid UUID of player.
      * @return Active username of uuid.
      */
     public String getActiveUsername(String uuid) {
-       return this.getDatabaseManager().getActiveUsername(uuid);
+        return this.getDatabaseManager().getActiveUsername(uuid);
+    }
+
+    /**
+     * Get user index.
+     *
+     * @param uuid UUID of the player.
+     * @return Index of the username.
+     */
+    public int getUserIndex(String uuid) {
+        return this.getDatabaseManager().getUI(uuid);
     }
 
 
